77d0ac1d633a8327c1ffffb4b68854f1a7212d7e
Lamm9 <harrylamm@icloud.com>
2023-05-20 10:42:19 -0700

    added loading screen

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PortfolioContainer from "./components/pc";
+import Loader from "./components/Loader";
+import './styles/App.css';
 
-const App = () => <PortfolioContainer />;
+const App = () => {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 6000);
+  }, []);
+  return (
+    <div className="App">{loading ? <Loader /> : <PortfolioContainer />}</div>
+  );
+};
 
 export default App;
---
+++ src/components/Loader.js
@@ -0,0 +1,36 @@
+import React from "react";
+import '../styles/App.css';
+import TextTransition, { presets } from 'react-text-transition';
+
+export default function Loader() {
+  const TEXTS = [
+    "a Web Developer",
+    "a Graphic Designer",
+    "a Digital Artist",
+    "here to help."
+  ];
+
+  const [index, setIndex] = React.useState(0);
+
+  React.useEffect(() => {
+    const intervalId = setInterval(() => {
+      setIndex(index => index + 1);
+    }, 1500);
+    return () => clearTimeout(intervalId);
+  }, []);
+
+  return (
+    <div className="loader">
+      <div className="container-fluid mt-5">
+        <div className="row mt-5 text-fancy">
+          <div className="col-3 text-end pe-2">
+            <h1 className="">I'm</h1>
+          </div>
+          <div className="col-8 text-start p-0">
+            <h1 className=""><TextTransition>{TEXTS[index % TEXTS.length]}</TextTransition></h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
